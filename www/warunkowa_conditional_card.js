9969a0c668ffc22a42850a2b51371610b0a0ff16
porebas <porebas@gmail.com>
2023-08-02 13:26:22 +0200

    Initial commit

---
+++ www/warunkowa_conditional_card.js
@@ -0,0 +1,50 @@
+class CustomConditionalCard extends HTMLElement {
+  set hass(hass) {
+    if (!this.content) {
+      const card = document.createElement('ha-card');
+      this.content = document.createElement('div');
+      this.content.style.padding = '0 16px 16px';
+      card.appendChild(this.content);
+      this.appendChild(card);
+    }
+
+    const entityId = this.config.entity;
+    const condition = this.config.condition;
+    const value = parseFloat(this.config.value);
+
+    const entityState = parseFloat(hass.states[entityId].state);
+
+    let shouldDisplay = false;
+    switch (condition) {
+      case 'above':
+        shouldDisplay = entityState > value;
+        break;
+      case 'below':
+        shouldDisplay = entityState < value;
+        break;
+    }
+
+    this.content.innerHTML = `
+      ${shouldDisplay ? this.config.card : ''}
+    `;
+  }
+
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error('You need to define an entity');
+    }
+    if (!config.condition) {
+      throw new Error('You need to define a condition');
+    }
+    if (!config.value) {
+      throw new Error('You need to define a value');
+    }
+    this.config = config;
+  }
+
+  getCardSize() {
+    return 1;
+  }
+}
+
+customElements.define('custom-conditional-card', CustomConditionalCard);

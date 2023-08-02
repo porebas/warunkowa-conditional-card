3ff8b6c5d16cb9fe79d2251f93b61ffac2cb6681
porebas <porebas@gmail.com>
2023-08-02 22:10:46 +0200

    Update warunkowa-conditional-card.js

--- warunkowa-conditional-card.js
+++ warunkowa-conditional-card.js
@@ -1,72 +1,59 @@
-customElements.whenDefined('card-tools').then(() => {
-  var ct = customElements.get('card-tools');
+import {
+  LitElement,
+  html,
+  css,
+} from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 
-  class WarunkowaConditionalCard extends ct.LitElement {
+class WarunkowaConditionalCard extends LitElement {
 
-    static get properties() {
-      return {
-        config: {},
-        hass: {},
-      };
-    }
-
-    setConfig(config) {
-      if (!config.entity) {
-        throw new Error('You need to define an entity');
-      }
-      if (!config.condition) {
-        throw new Error('You need to define a condition');
-      }
-      if (!config.value) {
-        throw new Error('You need to define a value');
-      }
-      if (!config.card) {
-        throw new Error('You need to define a card');
-      }
-
-      this.config = config;
-      this.card = ct.createCard(config.card);
-    }
-
-    shouldUpdate(changedProps) {
-      if (changedProps.has('hass')) {
-        const entityId = this.config.entity;
-        const entityState = parseFloat(this.hass.states[entityId].state);
-        const condition = this.config.condition;
-        const value = parseFloat(this.config.value);
-
-        let shouldDisplay = false;
-        switch (condition) {
-          case 'above':
-            shouldDisplay = entityState > value;
-            break;
-          case 'below':
-            shouldDisplay = entityState < value;
-            break;
-        }
-        this.shouldDisplay = shouldDisplay;
-        this.card.hass = this.hass;
-      }
-      return ct.LitElement.prototype.shouldUpdate.call(this, changedProps);
-    }
-
-    render() {
-      if (!this.shouldDisplay) {
-        return null;
-      }
-      return ct.LitHtml `
-        <ha-card>
-          ${ct.LitHtml `${this.card}`}
-        </ha-card>
-      `;
-    }
-
-    getCardSize() {
-      return this.card.getCardSize();
-    }
-
+  static get properties() {
+    return {
+      hass: {},
+      config: {},
+    };
   }
 
-  customElements.define('warunkowa-conditional-card', WarunkowaConditionalCard);
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error('You need to define an entity');
+    }
+    if (!config.condition) {
+      throw new Error('You need to define a condition');
+    }
+    if (!config.value) {
+      throw new Error('You need to define a value');
+    }
+    if (!config.card) {
+      throw new Error('You need to define a card');
+    }
 
-});
+    this.config = config;
+    this.card = config.card;
+  }
+
+  shouldUpdate(changedProps) {
+    if (changedProps.has('hass')) {
+      const entityId = this.config.entity;
+      const entityState = parseFloat(this.hass.states[entityId].state);
+      const condition = this.config.condition;
+      const value = parseFloat(this.config.value);
+
+      let shouldDisplay = false;
+      switch (condition) {
+        case 'above':
+          shouldDisplay = entityState > value;
+          break;
+        case 'below':
+          shouldDisplay = entityState < value;
+          break;
+      }
+      this.shouldDisplay = shouldDisplay;
+      this.card.hass = this.hass;
+    }
+    return LitElement.prototype.shouldUpdate.call(this, changedProps);
+  }
+
+  // Continue your code here...
+}
+
+customElements.define('warunkowa-conditional-card', WarunkowaConditionalCard);

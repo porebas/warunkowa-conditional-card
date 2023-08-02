e7e8e96be515d5621388a178e11d537b7572281a
porebas <porebas@gmail.com>
2023-08-02 16:57:50 +0200

    Update warunkowa-conditional-card.js

--- warunkowa-conditional-card.js
+++ warunkowa-conditional-card.js
@@ -1,15 +1,15 @@
 customElements.whenDefined('card-tools').then(() => {
   var ct = customElements.get('card-tools');
-  
+
   class WarunkowaConditionalCard extends ct.LitElement {
-  
+
     static get properties() {
       return {
         config: {},
         hass: {},
       };
     }
-  
+
     setConfig(config) {
       if (!config.entity) {
         throw new Error('You need to define an entity');
@@ -23,18 +23,18 @@ customElements.whenDefined('card-tools').then(() => {
       if (!config.card) {
         throw new Error('You need to define a card');
       }
-  
+
       this.config = config;
       this.card = ct.createCard(config.card);
     }
-  
+
     shouldUpdate(changedProps) {
       if (changedProps.has('hass')) {
         const entityId = this.config.entity;
         const entityState = parseFloat(this.hass.states[entityId].state);
         const condition = this.config.condition;
         const value = parseFloat(this.config.value);
-  
+
         let shouldDisplay = false;
         switch (condition) {
           case 'above':
@@ -49,10 +49,10 @@ customElements.whenDefined('card-tools').then(() => {
       }
       return ct.LitElement.prototype.shouldUpdate.call(this, changedProps);
     }
-  
+
     render() {
       if (!this.shouldDisplay) {
-        return ct.LitHtml ``;
+        return null;
       }
       return ct.LitHtml `
         <ha-card>
@@ -60,13 +60,13 @@ customElements.whenDefined('card-tools').then(() => {
         </ha-card>
       `;
     }
-  
+
     getCardSize() {
       return this.card.getCardSize();
     }
-  
+
   }
-  
+
   customElements.define('warunkowa-conditional-card', WarunkowaConditionalCard);
-  
+
 });
